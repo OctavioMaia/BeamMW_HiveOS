534d076a024b796937e5f439f308386d7ba4131d
OctavioMaia <octaviojmaia@gmail.com>
2019-01-05 00:11:21 +0000

    Release

---
+++ h-run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+
+install_miner() {
+	wget https://builds.beam.mw/mainnet/2019.01.03/Release/linux/opencl-miner-1.0.52.tar.gz
+        tar -zxf opencl-miner-1.0.52.tar.gz
+	rm opencl-miner-1.0.52.tar.gz
+} 
+
+
+[ -t 1 ] && . colors
+
+. h-manifest.conf
+
+[[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
+[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
+[[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
+CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
+[[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
+[[ ! -d ./beam-opencl.miner ]] && install_miner
+
+
+./beam-opencl-miner $(< /hive/miners/custom/$CUSTOM_NAME/$CUSTOM_NAME.conf) $@ 2>&1 | tee $CUSTOM_LOG_BASENAME.log
+
