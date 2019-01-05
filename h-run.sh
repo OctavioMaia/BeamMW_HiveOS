3b880ec5121c5c95ffcbc9584ba3357e15d649fb
OctavioMaia <octaviojmaia@gmail.com>
2019-01-05 19:45:27 +0000

    Update v1.0.1

--- h-run.sh
+++ h-run.sh
@@ -2,15 +2,6 @@
 
 cd `dirname $0`
 
-install_miner() {
-	mkdir beam_opencl
-	cd beam_opencl
-	wget https://github.com/OctavioMaia/BeamMW_HiveOS/releases/download/v1.1/beam_opencl.tar.gz
-        tar -zxf beam_opencl.tar.gz
-	rm beam_opencl.tar.gz
-} 
-
-
 [ -t 1 ] && . colors
 
 . h-manifest.conf
@@ -20,7 +11,5 @@ install_miner() {
 [[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
 CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
 [[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
-[[ ! -d ./beam-opencl.miner ]] && install_miner
-
 
 ./beam-opencl-miner $(< /hive/miners/custom/$CUSTOM_NAME/$CUSTOM_NAME.conf) $@ 2>&1 | tee $CUSTOM_LOG_BASENAME.log
---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -y
+sudo apt-get install gcc-4.9 -y
+sudo apt-get upgrade libstdc++6 -y
